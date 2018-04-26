26a475345a9f81b5b90c4dfe8583829a23dc310e
John-Philip Johansson <john-philip.johansson@episerver.com>
2018-04-26 15:33:51 +0200

    Example from blog post on wrapping elements

--- src/Alloy.Mvc.Template/Models/Pages/ArticlePage.cs
+++ src/Alloy.Mvc.Template/Models/Pages/ArticlePage.cs
@@ -1,4 +1,10 @@
-﻿namespace AlloyTemplates.Models.Pages
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace AlloyTemplates.Models.Pages
 {
     /// <summary>
     /// Used primarily for publishing news articles on the website
@@ -9,6 +15,21 @@
     [SiteImageUrl(Global.StaticGraphicsFolderPath + "page-type-thumbnail-article.png")]
     public class ArticlePage : StandardPage
     {
+        [Display(Description = "Name of person who wrote the article", GroupName = SystemTabNames.Content, Order = 10)]
+        public virtual string Author { get; set; }
 
+        [CultureSpecific]
+        [Display(Name = "Country", Description = "Country from which this article originated", GroupName = SystemTabNames.Content, Order = 30)]
+        public virtual string CountryOfOrigin { get; set; }
+
+        [UIHint(UIHint.Textarea)]
+        [Display(Name = "Summary", Description = "Short synopsis", GroupName = SystemTabNames.Content, Order = 40)]
+        [CultureSpecific]
+        public virtual string SummaryText { get; set; }
+
+        [UIHint(UIHint.Image)]
+        [Display(Name = "Image", Description = "Short synopsis", GroupName = SystemTabNames.Content, Order = 50)]
+        [CultureSpecific]
+        public virtual ContentReference SummaryImage { get; set; }
     }
 }
