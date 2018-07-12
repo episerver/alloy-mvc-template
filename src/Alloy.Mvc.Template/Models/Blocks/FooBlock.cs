360e67fb7a29626ff1dac2f0395558264d39f4e3
Bartosz Sekula <bartosz.sekula@episerver.com>
2018-07-12 14:30:34 +0200

    Added a definition for FooBlock which is the content type that we will migrate to.

---
+++ src/Alloy.Mvc.Template/Models/Blocks/FooBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.DataAbstraction;
+
+namespace AlloyTemplates.Models.Blocks
+{
+    [SiteContentType(GUID = "111CF12F-1F01-4EA0-922F-0778314DDAF0")]
+    public class FooBlock : SiteBlockData
+    {
+        [Display(Order = 1, GroupName = SystemTabNames.Content)]
+        public virtual string MyProperty { get; set; }
+    }
+}
